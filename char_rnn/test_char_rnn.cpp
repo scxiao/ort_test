dbc3a26910de3108d4980f26b34ceccfc6b48b6a
Shucai Xiao <Shucai.Xiao@amd.com>
2022-01-26 09:39:33 -0600

    update c++ code to fix build error

--- char_rnn/test_char_rnn.cpp
+++ char_rnn/test_char_rnn.cpp
@@ -2,15 +2,19 @@
 #include <iostream>
 #include <map>
 #include <cstdlib>
+#include <numeric>
+#include <algorithm>
+#include <climits>
 
 // onnx runtime include
 #include <core/session/onnxruntime_cxx_api.h>
-#include <core/framework/allocator.h>
+// #include <core/framework/allocator.h>
 #include <core/providers/migraphx/migraphx_provider_factory.h>
 
 std::string get_type_name(ONNXTensorElementDataType type)
 {
     static std::map<ONNXTensorElementDataType, std::string> table = {
+        {ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL, "bool"}, 
         {ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16, "half"}, 
         {ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, "float"},
         {ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE, "double"},
@@ -44,6 +48,15 @@ void print_vec(std::vector<T>& vec)
     std::cout << "}" << std::endl;
 }
 
+template<class T>
+void set_default_dims(std::vector<T>& vec)
+{
+    for (auto& v : vec) {
+        if (v < 0) v = 1;
+    }
+}
+
+
 int main(int argc, char **argv)
 {
     if (argc != 2)
@@ -75,6 +88,7 @@ int main(int argc, char **argv)
         Ort::TypeInfo info = sess.GetInputTypeInfo(i);
         auto tensor_info = info.GetTensorTypeAndShapeInfo();
         in_dims[i] = tensor_info.GetShape();
+        set_default_dims(in_dims[i]);
         auto onnx_type = tensor_info.GetElementType();
         std::cout << ", type: " << get_type_name(onnx_type) << ", shape = ";
         print_vec(in_dims[i]);
@@ -102,27 +116,28 @@ int main(int argc, char **argv)
         Ort::TypeInfo info = sess.GetOutputTypeInfo(i);
         auto tensor_info = info.GetTensorTypeAndShapeInfo();
         out_dims[i] = tensor_info.GetShape();
+        set_default_dims(out_dims[i]);
         auto onnx_type = tensor_info.GetElementType();
         std::cout << ", type: " << get_type_name(onnx_type) << ", shape = ";
         print_vec(out_dims[i]);
 
-        std::size_t elem_num = std::accumulate(in_dims[i].begin(), in_dims[i].end(), 1, std::multiplies<int64_t>());
+        std::size_t elem_num = std::accumulate(out_dims[i].begin(), out_dims[i].end(), 1, std::multiplies<int64_t>());
         output_data[i].resize(elem_num);
         outputs.push_back(Ort::Value::CreateTensor<float>(memory_info, output_data[i].data(), output_data[i].size(),
                     out_dims[i].data(), out_dims[i].size()));
     }
     std::cout << std::endl;
 
-std::cout << "Before execution...." << std::endl;
+    std::cout << "Start inference ...." << std::endl;
     sess.Run(Ort::RunOptions{nullptr}, input_names, inputs.data(), inputs.size(), output_names,
             outputs.data(), outputs.size());
-std::cout << "After execution...." << std::endl;
+    std::cout << "Finished inference ...." << std::endl;
 
-    std::cout << "outputs = " << std::endl;
+    std::cout << "outputs: " << std::endl;
     std::size_t i = 0;
     for (auto& output : output_data)
     {
-        std::cout << "Output " << i++ << " = " << std::endl;
+        std::cout << "Output_" << i++ << " = " << std::endl;
         print_vec(output);
         std::cout << std::endl;
     }
